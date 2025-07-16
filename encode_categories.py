ba14c6b47e6f380b97f38c757ed01db7733ab0c2
Nic Simms <nsimms@asu.edu>
2025-07-15 23:56:52 -0700

    add label encoding

--- encode_categories.py
+++ encode_categories.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 
 from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import LabelEncoder, OrdinalEncoder
 from scipy.sparse import issparse
 
 from gen_data import generate_fake_data
@@ -37,6 +38,15 @@ def sparse_vs_dense_df(data):
     return sparse_df, dense_df
 
 #Label Encoding
+def label_encoder(dataframe: pd.DataFrame):
+    encoder = LabelEncoder()
+    encoded_array = encoder.fit_transform(dataframe[["Color"]])
+    encoded_array = np.array(encoded_array).reshape(-1, 1)
+
+    transformed_df = pd.DataFrame(encoded_array, columns=['Color_LabelEncoded'])
+
+    return transformed_df
+
 
 #Frequency / Count Encoding
 
@@ -53,17 +63,19 @@ if __name__ == "__main__":
     fake_data = generate_fake_data(sample_size=30)
 
     encoded_data = one_hot_encoder(fake_data)
-
     #Uncomment to show the fake vs encoded.
     #print(fake_data.head())
     #print(encoded_data.head())
 
     array = fake_data[["Color"]]
-
     sparse_df, dense_df = sparse_vs_dense_df(array)
-
     #There are no differences between sparse and dense when showing. But some conversion is needed in the function.
     #print(sparse_df.head())
     #print(dense_df.head())
+
+    label_array = label_encoder(fake_data[["Color"]])
+    #Uncomment to show the fake vs encoded.
+    #print(fake_data.head())
+    #print(label_array.head())
 
     
